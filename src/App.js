dd9c41383e97a4ac693fa69c41a142ce7cf12e2b
Dyrits <dyrits@outlook.com>
2022-06-22 17:41:32 -0500

    Minor update.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { render } from "react-dom";
+import { StrictMode } from "react";
 
 import SearchParams from "./SearchParams";
 
 const App = () =>
-    <div>
-        <h1>Adopt Me!</h1>
-        <SearchParams />
-    </div>
+    <StrictMode>
+        <div>
+            <h1>Adopt Me!</h1>
+            <SearchParams />
+        </div>
+    </StrictMode>
+;
 
 render(<App />, document.querySelector("#root"));
